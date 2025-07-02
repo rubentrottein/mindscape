6c324522b178a95e2ec0a47834f52d892c30359b
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2025-07-03 01:52:00 +0200

    Erreur mineure corrigée

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
           <div className="flex flex-col gap-6">
             <h2 className="text-xl font-bold mb-2">Playlists de travail : </h2>
             <h3>Classique : Valses de Chopin</h3>  
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/64B-gW9FtY8?si=2pHAX1eM0Vb9ca6u" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/64B-gW9FtY8?si=2pHAX1eM0Vb9ca6u" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             <h3>Néo-Classique : Clair-Obscur Expedition 33</h3>  
             <iframe width="560" height="315" src="https://www.youtube.com/embed/LAQZfeETFbg?si=ZVp0Pj4gLSnw0dzh" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             <h3>Bebop : Jazz Noir</h3>  
